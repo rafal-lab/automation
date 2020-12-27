9e1deff7e4cbac2f0097d9ebf4172e305df78057
Rafał Frączek <rafffraczek98@gmail.com>
2020-12-27 20:17:44 +0100

    Adding speech recongnition and listener server

--- mail_sender.py
+++ mail_sender.py
@@ -1,9 +1,25 @@
 import smtplib
+import speech_recognition as sr
+import pyaudio
 
+#listener
+listener = sr.Recognizer()
+
+def get_info():
+    try:
+        with sr.Microphone() as source:
+            print('listening...')
+            listener.adjust_for_ambient_noise(source=source)
+            voice = listener.listen(source, )
+            info = listener.recognize_google(voice, language='pl-PL', show_all=True) #en-IN
+            print(info)
+    except OSError:
+        print('error')
 #creating server
-server = smtplib.SMTP('smtp.gmail.com', 587)
-server.starttls() #transport layer security
-server.login('jakismail.prosty@gmail.com', 'Haslo123') #username and password
-server.sendmail('jakismail.prosty@gmail.com',
-                'jakismail.prosty@gmail.com',
-                ' My message to someone')
+def send_email():
+    server = smtplib.SMTP('smtp.gmail.com', 587)
+    server.starttls() #transport layer security
+    server.login('jakismail.prosty@gmail.com', 'Haslo123') #username and password
+    server.sendmail('jakismail.prosty@gmail.com',
+                   'jakismail.prosty@gmail.com',
+                    ' My message to someone')
